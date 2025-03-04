8af075e47f41323b69abc56baa64a2d1a2f44820
iremsukaya <theiremsukaya@gmail.com>
2025-03-04 13:34:35 +0300

    Add files via upload

---
+++ arac_kullanimlari/arac_kullanimlari/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace arac_kullanimlari
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            button4 = new Button();
+            label9 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Verdana", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            button1.Location = new Point(12, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 79);
+            button1.TabIndex = 0;
+            button1.Text = "tıkla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 22.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label1.Location = new Point(12, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 43);
+            label1.TabIndex = 1;
+            label1.Text = "adınız:";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 121);
+            button2.Name = "button2";
+            button2.Size = new Size(190, 53);
+            button2.TabIndex = 2;
+            button2.Text = "yazdır";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(136, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            label2.Click += label2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            button3.Location = new Point(540, 180);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "bilgi getir";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Italic);
+            label3.Location = new Point(320, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(133, 46);
+            label3.TabIndex = 5;
+            label3.Text = "adınız:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Italic);
+            label4.Location = new Point(540, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(120, 46);
+            label4.TabIndex = 6;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Italic);
+            label5.Location = new Point(320, 55);
+            label5.Name = "label5";
+            label5.Size = new Size(187, 46);
+            label5.TabIndex = 7;
+            label5.Text = "soyadınız:";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Italic);
+            label6.Location = new Point(540, 55);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 46);
+            label6.TabIndex = 8;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Italic);
+            label7.Location = new Point(320, 116);
+            label7.Name = "label7";
+            label7.Size = new Size(225, 46);
+            label7.TabIndex = 9;
+            label7.Text = "messleginiz:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 24F, FontStyle.Bold | FontStyle.Italic);
+            label8.Location = new Point(540, 121);
+            label8.Name = "label8";
+            label8.Size = new Size(120, 46);
+            label8.TabIndex = 10;
+            label8.Text = "label8";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 291);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 11;
+            textBox1.Text = "irem";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 324);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 12;
+            button4.Text = "aktar";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(143, 314);
+            label9.Name = "label9";
+            label9.Size = new Size(50, 20);
+            label9.TabIndex = 13;
+            label9.Text = "label9";
+            label9.Click += label9_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(800, 472);
+            Controls.Add(label9);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "arac";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Label label2;
+        private Button button3;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private Button button4;
+        private Label label9;
+    }
+}
---
+++ arac_kullanimlari/arac_kullanimlari/Form1.cs
@@ -0,0 +1,61 @@
+namespace arac_kullanimlari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("merhaba badem");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "irem";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Text = "badem";
+            label6.Text = "kaya";
+            label8.Text = "mama yiyicisi";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            // textBox1.Text = "merhaba badem kaya";
+            label9.Text = textBox1.Text;
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
